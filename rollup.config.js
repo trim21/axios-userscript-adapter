5480135c1cbe9e0f00c9efccfc3f74c2401f34a8
Trim21 <trim21.me@gmail.com>
2022-06-01 09:11:43 +0800

    chore: build commonjs with tsc

--- rollup.config.js
+++ rollup.config.js
@@ -1,68 +1,37 @@
-// rollup.config.js
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-import del from "rollup-plugin-delete";
 
-const externals = [
-  "axios",
-  "axios/lib/core/createError",
-  "axios/lib/core/settle",
-  "axios/lib/helpers/buildURL",
-  "axios/lib/core/buildFullPath",
-  "axios/lib/helpers/parseHeaders",
-  "axios/lib/utils",
-];
+const globals = {
+  "axios": "axios"
+};
 
 const plugins = [
   commonjs(),
-  typescript({ tsconfig: "./tsconfig.base.json" }),
-  del({
-    targets: "dist/dist",
-    hook: "buildEnd",
-    verbose: true,
-  }),
+  typescript({ tsconfig: "./tsconfig.base.json" })
 ];
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "dist/axiosGmxhrAdapter.js",
-        format: "umd",
-        name: "axiosGmxhrAdapter",
-        exports: "default",
-        sourcemap: false,
-      },
-      {
-        file: "dist/axiosGmxhrAdapter.min.js",
-        format: "umd",
-        name: "axiosGmxhrAdapter",
-        exports: "default",
-        sourcemap: false,
-        plugins: [terser()],
-      },
-    ],
-    plugins,
-  },
-  {
-    input: "src/index.ts",
-    external: externals,
-    output: [
-      {
-        dir: "dist/esm",
-        format: "esm",
-        exports: "default",
-        sourcemap: true,
-      },
-      {
-        dir: "dist",
-        format: "commonjs",
-        exports: "default",
-        sourcemap: true,
-      },
-    ],
-    plugins,
-  },
-];
+export default {
+  input: "src/index.ts",
+  external: ["axios"],
+  output: [
+    {
+      file: "dist/axiosGmxhrAdapter.js",
+      format: "umd",
+      name: "axiosGmxhrAdapter",
+      exports: "default",
+      globals,
+      sourcemap: true
+    },
+    {
+      file: "dist/axiosGmxhrAdapter.min.js",
+      format: "umd",
+      name: "axiosGmxhrAdapter",
+      exports: "default",
+      sourcemap: true,
+      globals,
+      plugins: [terser()]
+    }
+  ],
+  plugins
+};
