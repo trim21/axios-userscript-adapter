dc1ccb55b6ba75615032abea3d115ac1df58b73e
Trim21 <trim21.me@gmail.com>
2022-06-15 12:29:54 +0800

    fix type

--- src/types/GM/request.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace GM {
-  interface Request {
-    /** Decode the response as specified type **/
-    responseType: "" | "arraybuffer" | "blob" | "document" | "json" | "text" | "stream" | undefined;
-  }
-}
